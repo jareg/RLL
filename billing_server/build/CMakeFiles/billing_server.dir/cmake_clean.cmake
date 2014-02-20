c2693d05d2dd0a9d6f34198a30d409a912cbe240
jareg <jareg@gmx.com>
2014-02-20 19:31:57 +0400

    nother first commit)))

---
+++ billing_server/build/CMakeFiles/billing_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/billing_server.dir/main.cpp.o"
+  "billing_server.pdb"
+  "billing_server"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/billing_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
